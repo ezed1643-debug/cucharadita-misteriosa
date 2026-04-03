362e65b604d988877a22ed9a4ec4cce47036f715
Ezequiel <ezed1643@gmail.com>
2026-04-03 14:41:47 -0300

    Creando y configurando tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-import { nextui } from "@nextui-org/react";
+const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
